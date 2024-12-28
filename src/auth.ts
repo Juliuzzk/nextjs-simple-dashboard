3f60b228915279a5e105c813d343422620099088
Julio Esteban Cáceres Sanzana <Julio@Macbook-M1-Pro.local>
2024-12-28 16:19:57 -0300

    update authts

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,17 @@
+import { loadQuery } from '@/lib/load-query';
+import { query } from '@/lib/db';
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 import Credentials from 'next-auth/providers/credentials';
 import PostgresAdapter from '@auth/pg-adapter';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { Pool } from 'pg';
+
+// Cargar las queries SQL
+const selectUserSQL = loadQuery(
+	'src/app/api/auth/login/queries/LoginSelectUser.sql'
+);
 
 const pool = new Pool({
 	host: process.env.PG_HOST,
@@ -27,15 +36,35 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 				password: {},
 			},
 			authorize: async (credentials) => {
-				let user = null;
+				// Buscar al usuario en la base de datos
+				const { rows } = await query(selectUserSQL, [credentials.email]);
+				const user = rows[0];
 
+				if (!user) {
+					throw new Error('User Not Found');
+				}
 				console.log('Credentials: ', credentials);
 
-				throw new Error('Invalid check my Throw.');
-				// return user object with their profile data
+				const isPasswordValid = await bcrypt.compare(
+					credentials.password as string,
+					user.password
+				);
+				if (!isPasswordValid) {
+					throw new Error('User Not Found');
+				}
 
 				return user;
 			},
+			// authorize: async (credentials) => {
+			// 	let user = null;
+			//
+			// 	console.log('Credentials: ', credentials);
+			//
+			// 	throw new Error('Invalid check my Throw.');
+			// 	// return user object with their profile data
+			//
+			// 	return user;
+			// },
 		}),
 	],
 });
